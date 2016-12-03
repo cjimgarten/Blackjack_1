b118e4bb780aa800f9c67e7ecedee519165faba4
Chris Imgarten <cjimgarten@gmail.com>
2016-12-03 12:35:15 -0600

    renamed Blackjack_1.java -> Main.java
    renamed BlackjackApplication.java -> Blackjack_1.java

---
+++ src/com/cjimgarten/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Main.java
+ * 
+ * created: 12-03-2016
+ * modified: 12-03-2016
+ * 
+ * launches Blackjack_1 application
+ */
+
+package com.cjimgarten;
+
+public class Main {
+	
+	/**
+	 * launch the application
+	 */
+	public static void main(String[] args) {
+		Blackjack_1 app = new Blackjack_1();
+		app.startApplication();
+	}
+}
